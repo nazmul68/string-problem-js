92d255a735af60ed69029a0b9fd005d92320bedb
unknown <nazmul sujon>
2022-08-08 12:38:21 +0600

    return part added

--- if-else-fucntionWithPmtrs.js
+++ if-else-fucntionWithPmtrs.js
@@ -1,18 +1,22 @@
-// function getMax(num1,num2,num3){
-//     if(jim> dela && jim > chinku){
-//         console.log('Jim get the cake');
-//     }
-//     else if(dela>jim && dela > chinku){
-//         console.log('Dela get the cake');
-//     }
-//     else{
-//         console.log('Chinku get the cake');
-//     }
-// }
-// const jim = 90;
-// const dela = 92;
-// const chinku = 95;
-// getMax(jim,dela,chinku);
+function getMax(num1,num2,num3){
+    if(jim> dela && jim > chinku){
+        return 'Jim get the cake'
+    }
+    
+    else if(dela>jim && dela > chinku){
+        return 'Dela get the cake'
+    }
+    else{
+        return 'Chinku get the cake'
+    }
+
+}
+const jim = 90;
+const dela = 92;
+const chinku = 95;
+const result = getMax(jim,dela,chinku);
+console.log(result);
+
 
 
 
@@ -22,13 +26,13 @@
 // //------- using Math.min or max -------// //
 
 
- const jim = 90;
- const dela = 92;
- const chinku = 95;
-function getMax(num1,num2,num3){
-    console.log('First position who get:', Math.max(num1,num2,num3));
-    console.log('Last position who get:', Math.min(num1,num2,num3));
+//  const jim = 90;
+//  const dela = 92;
+//  const chinku = 95;
+// function getMax(num1,num2,num3){
+//     console.log('First position who get:', Math.max(num1,num2,num3));
+//     console.log('Last position who get:', Math.min(num1,num2,num3));
 
  
-}
-getMax(jim,dela,chinku);
+// }
+// getMax(jim,dela,chinku);
